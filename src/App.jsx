efdb5070312a6f53c492877133a5442bee878a8b
HungNguyen <nguyen.phathung123@gmail.com>
2023-10-04 19:31:24 +0700

    Fix bug can't run Tailwind + Can use npm start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import { Routes, Route, Link } from "react-router-dom"
-import Login from './Login'
+
+import Contact from './pages/test/contact'
+import Info from './pages/test/info'
+import Main from './pages/test/main'
+
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 
 function App() {
   return (
-    <div style={{"backgroundImage": "url('C:/Users/PC/OneDrive/Máy tính/CNPMNC-Web/src/assets/image.jpg'"}}>
-        <Login/>
+    <div style={{ "backgroundImage": "url('C:/Users/PC/OneDrive/Máy tính/CNPMNC-Web/src/assets/image.jpg'" }}>
+
+      <Link to="/Contact">Contact</Link>
+      <br></br>
+      <Link to="/Info">Info</Link>
+      <br></br>
+      <Link to="/Main">Main</Link>
+      <br></br>
+      <Link to="/Login">Login</Link>
+      <br></br>
+      <Link to="/Register">Register</Link>
+
+      <p>Nội dung các trang sẽ ở đây</p>
+      <br></br>
+      <Routes>
+        @*Để test backend*@
+        <Route path="/" element={<p className="text-3xl font-bold underline">Hello, World</p>} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Info" element={<Info />} />
+        <Route path="/Main" element={<Main />} />
+
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     </div>
 
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,29 @@
-const Login = () =>{
+import { Link } from "react-router-dom";
+
+
+export default function Login() {
     return (
         <div>
             <div>
                 <h1>Login</h1>
                 <div>
-                    <input type="email"/>
+                    <input type="email" />
                     <label htmlFor="">Your email</label>
                 </div>
                 <div>
-                    <input type="email"/>
+                    <input type="email" />
                     <label htmlFor="">Your email</label>
                 </div>
                 <div>
-                    <input type="checkbok" name ="" id =""/>
+                    <input type="checkbok" name="" id="" />
                     <label htmlFor="Remember me"></label>
                     <span>Quên mật khẩu?</span>
                 </div>
                 <button type="submit">Login</button>
                 <div>
-                    <span>Người dùng mới? <Link to ='Register'>Create an Account</Link></span>
+                    <span>Người dùng mới? <Link to='/Register'>Create an Account</Link></span>
                 </div>
             </div>
         </div>
     )
 }
-
-export default Login;
