e2cb8a070d9302682d71b4c1de723b8f413a19f4
TDAT0105 <ngotrungdat123@gmail.com>
2023-10-04 10:39:08 +0700

    dang nhap

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom"
-import Contact from "./pages/contact"
-import Info from "./pages/info"
-import Main from "./pages/main";
+import Login from './Login'
 
-export default function App() {
+
+function App() {
   return (
-    <div className="App">
-      <div>
-        <Link to="/Contact">Contact</Link>
-        <br></br>
-        <Link to="/Info">Info</Link>
-        <br></br>
-        <Link to="/Main">Main</Link>
-
-        <Routes>
-          <Route path="/" element={<p>Hello, World</p>} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Info" element={<Info />} />
-          <Route path="/Main" element={<Main />} />
-        </Routes>
-      </div>
+    <div style={{"backgroundImage": "url('C:/Users/PC/OneDrive/Máy tính/CNPMNC-Web/src/assets/image.jpg'"}}>
+        <Login/>
     </div>
+
   )
 }
+
+export default App
 
 //Link to thay thế cho href
 //Route dùng để render ra thực thể
---
+++ src/Login.jsx
@@ -0,0 +1,28 @@
+const Login = () =>{
+    return (
+        <div>
+            <div>
+                <h1>Login</h1>
+                <div>
+                    <input type="email"/>
+                    <label htmlFor="">Your email</label>
+                </div>
+                <div>
+                    <input type="email"/>
+                    <label htmlFor="">Your email</label>
+                </div>
+                <div>
+                    <input type="checkbok" name ="" id =""/>
+                    <label htmlFor="Remember me"></label>
+                    <span>Quên mật khẩu?</span>
+                </div>
+                <button type="submit">Login</button>
+                <div>
+                    <span>Người dùng mới? <Link to ='Register'>Create an Account</Link></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
