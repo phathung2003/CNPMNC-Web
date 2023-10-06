116ca50338c2669ddcc7c174a2c77d3928c45ab4
TDAT0105 <ngotrungdat123@gmail.com>
2023-10-06 13:47:03 +0700

    them form dang nhap

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import Register from './pages/Register'
 
 function App() {
   return (
-    <div style={{ "backgroundImage": "url('C:/Users/PC/OneDrive/Máy tính/CNPMNC-Web/src/assets/image.jpg'" }}>
-
+    <div>
+        
       <Link to="/Contact">Contact</Link>
       <br></br>
       <Link to="/Info">Info</Link>
@@ -22,20 +22,22 @@ function App() {
       <br></br>
       <Link to="/Register">Register</Link>
 
-      <p>Nội dung các trang sẽ ở đây</p>
-      <br></br>
-      <Routes>
-        @*Để test backend*@
-        <Route path="/" element={<p className="text-3xl font-bold underline">Hello, World</p>} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Info" element={<Info />} />
-        <Route path="/Main" element={<Main />} />
+          <div className='flex justify-center items-center  ' >
+          
+            <p>Nội dung các trang sẽ ở đây</p>
+            <br></br>
+            <Routes>
+              @*Để test backend*@
+              <Route path="/" element={<p className="text-3xl font-bold underline">Hello, World</p>} />
+              <Route path="/Contact" element={<Contact />} />
+              <Route path="/Info" element={<Info />} />
+              <Route path="/Main" element={<Main />} />
 
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-      </Routes>
+              <Route path="/Login" element={<Login />} />
+              <Route path="/Register" element={<Register />} />
+            </Routes>
+          </div>
     </div>
-
   )
 }
 
@@ -43,3 +45,4 @@ export default App
 
 //Link to thay thế cho href
 //Route dùng để render ra thực thể
+//bò ảnh vào file public 
