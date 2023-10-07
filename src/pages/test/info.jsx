fa7c091bb04047d03277fe38ee66645ea7450705
TDAT0105 <ngotrungdat123@gmail.com>
2023-10-07 11:21:45 +0700

    Fix merging from master

--- src/pages/test/info.jsx
+++ src/pages/test/info.jsx
@@ -6,7 +6,6 @@ const api = import.meta.env.VITE_API_INFO
 export default function Info() {
     const [userInfo, setInfo] = useState([])
 
-<<<<<<< HEAD
     if (api !== undefined) {
         useEffect(() => {
             axios.get(api)
@@ -17,41 +16,8 @@ export default function Info() {
     else {
         console.log("Bạn thiếu file .env hoặc file .env không hợp lệ để truyền dữ liệu")
     }
-=======
-    useEffect(() => {
-        axios.get(api)
-            .then(info => setInfo(info.data))
-            .catch(err => console.log(err))
-    }, [])
->>>>>>> 974def3 (Save Login Progress)
 
     return (
-        <div>
-            <p>Info Pages</p>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Họ tên</th>
-                        <th>Email</th>
-                        <th>Mật khẩu</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {
-                        userInfo.map(info => {
-                            return <tr>
-                                <td>{info.name}</td>
-                                <td>{info.email}</td>
-                                <td>{info.password}</td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-
         <div>
             <p>Info Pages</p>
 
