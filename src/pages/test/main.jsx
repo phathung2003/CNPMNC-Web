3beb8da9f3ede9f74becc2ec8559e5bb1ab1c58a
Haki <duytu1232@gmail.com>
2023-10-07 13:26:18 +0700

    Fix merge error

--- src/pages/test/main.jsx
+++ src/pages/test/main.jsx
@@ -6,7 +6,51 @@ import { Link, useNavigate } from 'react-router-dom';
 
 export default function Main() {
     return (
+        <div>
+            <p>Main Pages</p>
+            <section class="bg-gray-50 dark:bg-gray-900">
+                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Đăng nhập
+                            </h1>
+                            <form class="space-y-4 md:space-y-6" onSubmit={handelSubmit}>
 
-        <p>Main Pages</p>
+                                <div>
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <input
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        type='email'
+                                        placeholder="Hãy nhập email"
+                                        autoComplete='off'
+                                        name="emailInput"
+                                        onChange={(input) => setEmail(input.target.value)}
+                                    />
+                                </div>
+
+                                <div>
+                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mật khẩu</label>
+                                    <input
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        type='password'
+                                        placeholder="••••••••"
+                                        autoComplete='off'
+                                        name='password'
+                                        onChange={(input) => setPass(input.target.value)}
+                                    />
+                                </div>
+
+                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Đăng nhập</button>
+                            </form>
+
+                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Chưa có tài khoản <Link to="/contact" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Đăng ký</Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 }
