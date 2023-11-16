e1eb14b33efc43b20b7aaa7e585c9f42fb7a4646
TDAT0105 <ngotrungdat123@gmail.com>
2023-11-17 01:53:25 +0700

    fix date staff

--- src/pages/StaffManager/MainStaff.jsx
+++ src/pages/StaffManager/MainStaff.jsx
@@ -106,6 +106,108 @@ import "../../css/table.css"
 import Value from "../../backend/StaffManager/staffMain"
 import Delete from "../../backend/StaffManager/staffDelete";
 import { useEffect, useState } from 'react';
+import { formatDate } from '@fullcalendar/core';
+
+export default function Info() {
+    const navigate = useNavigate();
+    const [search, setSearch] = useState("");
+    const staffInfo = Value();
+    
+    return (
+        <div>
+            <h2>Quản lý nhân viên</h2>
+            <div className="d-flex justify-content-between mb-3">
+                <div className="row">
+                    <div className="input-group">
+
+                        <div className="form-outline" style={{ marginRight: "5px" }}>
+                            <input type="text" className="form-control border border-secondary" placeholder="Tìm kiếm" onChange={(e) => setSearch(e.target.value.toLowerCase())} />
+                        </div>
+                        <div>
+                            <button className="btn btn-primary" type="submit">
+                                <i><SearchIcon /></i>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="d-flex justify-content-end">
+                    <button className="btn btn-success" onClick={(e) => navigate("/StaffAdd")}>Thêm nhân viên</button>
+                </div>
+            </div>
+
+            <div id="table-scroll" className="table-scroll">
+                <table id="main-table" className="main-table" style={{ width: "100%" }}>
+                    <thead>
+                        <tr style={{ textAlign: "center" }}>
+                            <th>ID</th>
+                            <th>Hình Ảnh</th>
+<<<<<<< HEAD
+                            <th>Hình CMND</th>
+=======
+>>>>>>> c0566bb (fix mainStaff)
+                            <th>Hình CMND</th>
+                            <th>Tên nhân viên</th>
+                            <th>Ngày Sinh</th>
+                            <th>Địa chi</th>
+                            
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            staffInfo.filter((item) => {
+                                if (search === "") return item;                              
+                                return item.ID.toLowerCase().includes(search) ||
+                                    item.TenNV.toLowerCase().includes(search) ||
+                                    item.NgaySinh.toLowerCase().includes(search) ||
+                                    item.CMND.toLowerCase().includes(search) ||
+                                    item.DiaChi.toLowerCase().includes(search) 
+                                    
+                            }).map(info => {
+                                return <tr key={info._id}>
+<<<<<<< HEAD
+                                    <td align="center" style={{ width: "5%" }}>{info.IDNV}</td>
+                                    <td align="center" style={{ verticalAlign: "middle", width: "15%" }}><img src={`${info.Avatar}`}></img></td>
+                                    <td align="center" style={{ verticalAlign: "middle", width: "15%" }}><img src={`${info.HinhCMND}`}></img></td>
+                                    <td style={{ textAlign: "center" }}>{info.TenNV}</td>
+                                    <td style={{ textAlign: "center" }}>{formatDate(info.NgaySinh)}</td>
+                                    <td style={{ textAlign: "center" }}>{info.DiaChi}</td>
+                                    
+=======
+                                    <td align="center" style={{ width: "5%" }}>{info.IDNV}</td>
+                                    <td align="center" style={{ verticalAlign: "middle", width: "15%" }}><img src={`${info.Avatar}`}></img></td>
+                                    <td align="center" style={{ verticalAlign: "middle", width: "15%" }}><img src={`${info.HinhCMND}`}></img></td>
+                                    <td style={{ textAlign: "center" }}>{info.TenNV}</td>
+                                    <td style={{ textAlign: "center" }}>{formatDate(info.NgaySinh)}</td>
+                                    <td style={{ textAlign: "center" }}>{info.DiaChi}</td>
+                                    {console.log("hello")}
+                                    <td>
+                                        <button className="btn btn-primary" onClick={(e) => navigate("/StaffEdit", { state: info })}>Chi tiết</button>
+                                        <button className="btn btn-danger ml-2" onClick={(e) => Delete(info._id, info.Avatar)}>Xoá bài</button>
+                                        
+                                    </td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div >
+
+        </div>
+
+    );
+}
+import { useNavigate } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+
+import "../../css/table.css"
+import Value from "../../backend/StaffManager/staffMain"
+import Delete from "../../backend/StaffManager/staffDelete";
+import { useEffect, useState } from 'react';
+import { formatDate } from '@fullcalendar/core';
 
 export default function Info() {
     const navigate = useNavigate();
@@ -167,7 +269,7 @@ export default function Info() {
                                     <td align="center" style={{ verticalAlign: "middle", width: "15%" }}><img src={`${info.Avatar}`}></img></td>
                                     <td align="center" style={{ verticalAlign: "middle", width: "15%" }}><img src={`${info.HinhCMND}`}></img></td>
                                     <td style={{ textAlign: "center" }}>{info.TenNV}</td>
-                                    <td style={{ textAlign: "center" }}>{info.NgaySinh}</td>
+                                    <td style={{ textAlign: "center" }}>{formatDate(info.NgaySinh)}</td>
                                     <td style={{ textAlign: "center" }}>{info.DiaChi}</td>
                                     
                                     <td>
